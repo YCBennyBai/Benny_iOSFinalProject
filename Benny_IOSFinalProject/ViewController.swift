848d26edc32a4373cf5dcfc5f3ecddf2f56ca5ce
YCBennyBai <ycbennybai@gmail.com>
2022-04-04 12:31:29 -0400

    Initial Commit

---
+++ Benny_IOSFinalProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Benny_IOSFinalProject
+//
+//  Created by Yuechen Bai on 4/4/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
